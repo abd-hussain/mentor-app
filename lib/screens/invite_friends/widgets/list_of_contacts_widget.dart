eea4cdb8aa08593ffd6fc2c15fd6a27858002cf2
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2023-11-18 20:49:53 +0300

    .

--- lib/screens/invite_friends/widgets/list_of_contacts_widget.dart
+++ lib/screens/invite_friends/widgets/list_of_contacts_widget.dart
@@ -2,15 +2,14 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
-// import 'package:flutter_sms/flutter_sms.dart';
+import 'package:flutter_sms/flutter_sms.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:mentor_app/shared_widget/custom_button.dart';
-// import 'package:mentor_app/utils/constants/constant.dart';
+import 'package:mentor_app/utils/constants/constant.dart';
 
 class ListOfContactsWidget extends StatefulWidget {
   final List<Contact> contacts;
-  const ListOfContactsWidget({Key? key, required this.contacts})
-      : super(key: key);
+  const ListOfContactsWidget({Key? key, required this.contacts}) : super(key: key);
 
   @override
   State<ListOfContactsWidget> createState() => _ListOfContactsWidgetState();
@@ -21,8 +20,7 @@ class _ListOfContactsWidgetState extends State<ListOfContactsWidget> {
 
   @override
   void initState() {
-    listOfCheckboxInContact =
-        List<bool>.generate(widget.contacts.length, (i) => false);
+    listOfCheckboxInContact = List<bool>.generate(widget.contacts.length, (i) => false);
     super.initState();
   }
 
@@ -36,21 +34,18 @@ class _ListOfContactsWidgetState extends State<ListOfContactsWidget> {
                 itemCount: widget.contacts.length,
                 itemBuilder: (BuildContext context, int index) {
                   Uint8List? image = widget.contacts[index].photo;
-                  String num = (widget.contacts[index].phones.isNotEmpty)
-                      ? (widget.contacts[index].phones.first.number)
-                      : "--";
+                  String num =
+                      (widget.contacts[index].phones.isNotEmpty) ? (widget.contacts[index].phones.first.number) : "--";
                   return ListTile(
                       leading: (widget.contacts[index].photo == null)
                           ? const CircleAvatar(child: Icon(Icons.person))
                           : CircleAvatar(backgroundImage: MemoryImage(image!)),
-                      title: Text(
-                          "${widget.contacts[index].name.first} ${widget.contacts[index].name.last}"),
+                      title: Text("${widget.contacts[index].name.first} ${widget.contacts[index].name.last}"),
                       subtitle: Text(num),
                       trailing: Checkbox(
                         value: listOfCheckboxInContact[index],
                         onChanged: (value) {
-                          listOfCheckboxInContact[index] =
-                              !listOfCheckboxInContact[index];
+                          listOfCheckboxInContact[index] = !listOfCheckboxInContact[index];
                           setState(() {});
                         },
                       ),
@@ -69,12 +64,12 @@ class _ListOfContactsWidgetState extends State<ListOfContactsWidget> {
                 }
               }
 
-              // if (await canSendSMS()) {
-              //   if (context.mounted) {
-              //     String message = "${AppLocalizations.of(context)!.smsmessage} ${AppConstant.appLink}";
-              //     await sendSMS(message: message, recipients: recipents, sendDirect: false);
-              //   }
-              // }
+              if (await canSendSMS()) {
+                if (context.mounted) {
+                  String message = "${AppLocalizations.of(context)!.smsmessage} ${AppConstant.appLink}";
+                  await sendSMS(message: message, recipients: recipents, sendDirect: false);
+                }
+              }
             },
           ),
           const SizedBox(height: 20)
