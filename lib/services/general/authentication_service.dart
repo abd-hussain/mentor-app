d819458264fe0137f3704c3a0b327ae42bafb5ea
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2024-01-01 16:42:03 +0300

    handle iOS

--- lib/services/general/authentication_service.dart
+++ lib/services/general/authentication_service.dart
@@ -5,24 +5,20 @@ import 'package:mentor_app/models/authentication_models.dart';
 import 'package:mentor_app/utils/mixins.dart';
 
 class AuthenticationService with Service {
-  final LocalAuthentication? localAuthentication =
-      locator<LocalAuthentication>();
+  final LocalAuthentication? localAuthentication = locator<LocalAuthentication>();
 
   // Check if any hardware regarding authentication is available
   Future<bool> isBiometricAvailable() async {
-    return await localAuthentication!.canCheckBiometrics &&
-        await localAuthentication!.isDeviceSupported();
+    return await localAuthentication!.canCheckBiometrics && await localAuthentication!.isDeviceSupported();
   }
 
   // if authentication should be allowed
-  Future<bool> shouldAllowBiometricAuthenticationToContinue(
-      TargetPlatform? platform) async {
+  Future<bool> shouldAllowBiometricAuthenticationToContinue(TargetPlatform? platform) async {
     if (platform == TargetPlatform.iOS) {
       return Future.value(true);
     } else if (platform == TargetPlatform.android) {
       // We need to check the biometric types available, if finger print is available, return true, otherwise, return false
-      final listOfBiometricsAvailable =
-          await localAuthentication!.getAvailableBiometrics();
+      final listOfBiometricsAvailable = await localAuthentication!.getAvailableBiometrics();
       var shouldAuthenticate = false;
       if (listOfBiometricsAvailable.isNotEmpty) {
         shouldAuthenticate = true;
@@ -35,41 +31,26 @@ class AuthenticationService with Service {
 
   // Process of authentication user using
   // biometrics.
-  Future<AuthenticationResult> authenticateUser(
-      String localizedAuthenticationMessage) async {
-    print("authenticateUser");
+  Future<AuthenticationResult> authenticateUser(String localizedAuthenticationMessage) async {
     return localAuthentication!
         .authenticate(
-      options: const AuthenticationOptions(
-          biometricOnly: true, useErrorDialogs: true, stickyAuth: true),
+      options: const AuthenticationOptions(biometricOnly: true, useErrorDialogs: true, stickyAuth: true),
       localizedReason: localizedAuthenticationMessage,
     )
         .then((value) async {
-      print("authenticateUser then");
-
-      return AuthenticationResult(
-          success: value, errorMessage: value ? "" : "Authentication Failed");
+      return AuthenticationResult(success: value, errorMessage: value ? "" : "Authentication Failed");
     }).catchError((error) async {
-      print("authenticateUser catchError");
-      print(error.toString());
-
-      return AuthenticationResult(
-          success: false, errorMessage: error.toString());
+      return AuthenticationResult(success: false, errorMessage: error.toString());
     });
   }
 
-  Future<AuthenticationBiometricType> getAvailableBiometricTypes(
-      TargetPlatform? platform) async {
+  Future<AuthenticationBiometricType> getAvailableBiometricTypes(TargetPlatform? platform) async {
     // Note : if isAvailable = false then we ignore the BiometricType
 
-    AuthenticationBiometricType? response = AuthenticationBiometricType(
-        isAvailable: false, type: BiometricType.iris);
-    return await localAuthentication!
-        .getAvailableBiometrics()
-        .then((listOfBiometrics) {
+    AuthenticationBiometricType? response = AuthenticationBiometricType(isAvailable: false, type: BiometricType.iris);
+    return await localAuthentication!.getAvailableBiometrics().then((listOfBiometrics) {
       for (final biometricType in listOfBiometrics) {
-        response =
-            AuthenticationBiometricType(isAvailable: true, type: biometricType);
+        response = AuthenticationBiometricType(isAvailable: true, type: biometricType);
       }
 
       return Future.value(response);
